25b20690cabb2e35a215f02df204b859ed9453a4
allexwentworthok <wentapps.co@gmail.com>
2020-08-21 11:21:26 -0300

    Cambios en el tslint.json

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -15,37 +15,35 @@ export class Tab2Page {
   @ViewChild('BarChartAnual', {static: false}) BarChartAnual;
   @ViewChild('BarChartDiarioCategorias', {static: false}) BarChartDiarioCategorias;
 
-  BarsDiario:any;
-  BarsDiarioCat:any;
-  BarsAnual:any;
-  BarsSemestral:any;
+  BarsDiario: any;
+  BarsDiarioCat: any;
+  BarsAnual: any;
+  BarsSemestral: any;
   colorArray: any;
 
 
-  apiDiario:any;
-  apiAnual:any;
-  apiSemestral:any;
-  apiDiarioCategoria:any;
-  logos:string;
-  constructor(private http:HttpClient, public userService: UserService) {}
-
-  
+  apiDiario: any;
+  apiAnual: any;
+  apiSemestral: any;
+  apiDiarioCategoria: any;
+  logos: string;
+  constructor( private http: HttpClient, public userService: UserService) {}
 
   ionViewDidEnter() {
 
-    this.createBarChartSemanal()
-    this.createBarChartSemestral()
-    this.createDiarioCategorias()
+    this.createBarChartSemanal();
+    this.createBarChartSemestral();
+    this.createDiarioCategorias();
 
   }
 
 
-  ionViewWillEnter(){
+  ionViewWillEnter() {
     this.getLogo();
-    this.var_anual()
-    this.var_diarioCategorias()
-    this.var_semanal()
-    this.var_semestral()
+    this.var_anual();
+    this.var_diarioCategorias();
+    this.var_semanal();
+    this.var_semestral();
   }
   getLogo() {
     // Get saved list of students
@@ -56,39 +54,38 @@ export class Tab2Page {
   }
 
     var_semanal() {
-      const my_url = 'https://vigorous-chandrasekhar-2ee519.netlify.app/apiDiario.json'
+      const my_url = 'https://vigorous-chandrasekhar-2ee519.netlify.app/apiDiario.json';
       this.http.get(my_url).subscribe(data => {
         console.log(data);
         this.apiDiario = data;
         this.createBarChartSemanal();
-      })
+      });
     }
 
-    var_diarioCategorias(){
-      const my_url = 'https://vigorous-chandrasekhar-2ee519.netlify.app/apiCategoriasDiarias.json'
+    var_diarioCategorias() {
+      const my_url = 'https://vigorous-chandrasekhar-2ee519.netlify.app/apiCategoriasDiarias.json';
       this.http.get(my_url).subscribe(data => {
         console.log(data);
         this.apiDiarioCategoria = data;
         this.createDiarioCategorias();
-      })
+      });
     }
 
-    var_semestral(){
-      const my_url = 'https://vigorous-chandrasekhar-2ee519.netlify.app/apiSemestral.json'
+    var_semestral() {
+      const my_url = 'https://vigorous-chandrasekhar-2ee519.netlify.app/apiSemestral.json';
       this.http.get(my_url).subscribe(data => {
         console.log(data);
         this.apiSemestral = data;
         this.createBarChartSemestral();
-      })
+      });
     }
 
-    var_anual(){
-      const my_url = ''
+    var_anual() {
+      const my_url = 'https://vigorous-chandrasekhar-2ee519.netlify.app/apiSemestral.json';
       this.http.get(my_url).subscribe(data => {
         console.log(data);
         this.apiDiarioCategoria = data;
-        //this.createBarChartAnual();
-      })
+      });
     }
 
 
@@ -96,29 +93,29 @@ export class Tab2Page {
 
 
   createBarChartSemanal() {
-    let ctx = this.BarChartSemanal.nativeElement
+    const ctx = this.BarChartSemanal.nativeElement;
     ctx.height = 400;
-    this.BarsDiario = new Chart(ctx,{
+    this.BarsDiario = new Chart(ctx, {
     type: 'bar',
     data: {
       labels: this.apiDiario && this.apiDiario.labels,
       datasets: [
         {
-          label: "Miles de pesos",
+          label: 'Miles de pesos',
           data: this.apiDiario && this.apiDiario.values,
           backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(153, 102, 255, 0.2)"
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)'
           ],
           borderColor: [
-            "rgba(255,99,132,1)",
-            "rgba(54, 162, 235, 1)",
-            "rgba(255, 206, 86, 1)",
-            "rgba(75, 192, 192, 1)",
-            "rgba(153, 102, 255, 1)"
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)'
           ],
           borderWidth: 1
         }
@@ -135,57 +132,55 @@ export class Tab2Page {
         ]
       }
     }
-  })
+  });
 }
 
-  
-
   createDiarioCategorias() {
-    let ctx = this.BarChartDiarioCategorias.nativeElement
+    const ctx = this.BarChartDiarioCategorias.nativeElement;
     ctx.height = 400;
-    this.BarsDiarioCat = new Chart(ctx,{
-    type: "doughnut",
+    this.BarsDiarioCat = new Chart(ctx, {
+    type: 'doughnut',
     data: {
       labels:  this.apiDiarioCategoria && this.apiDiarioCategoria.labels,
       datasets: [
         {
-          label: "# Miles de pesos",
+          label: '# Miles de pesos',
           data: this.apiDiarioCategoria && this.apiDiarioCategoria.values,
           backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)"
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)'
           ],
-          hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"]
+          hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
         }
       ]
     }
-  })}
+  }); }
 
   createBarChartSemestral() {
-    let ctx = this.BarChartSemestral.nativeElement
+    const ctx = this.BarChartSemestral.nativeElement;
     ctx.height = 400;
-    this.BarsSemestral = new Chart(ctx,{
-    type: "line",
+    this.BarsSemestral = new Chart(ctx, {
+    type: 'line',
     data: {
       labels: this.apiSemestral && this.apiSemestral.labels ,
       datasets: [
         {
-          label: "Síntesis de Recaudación Histórico",
+          label: 'Síntesis de Recaudación Histórico',
           fill: false,
           lineTension: 0.1,
-          backgroundColor: "rgba(75,192,192,0.4)",
-          borderColor: "rgba(75,192,192,1)",
-          borderCapStyle: "butt",
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
           borderDash: [],
           borderDashOffset: 0.0,
-          borderJoinStyle: "miter",
-          pointBorderColor: "rgba(75,192,192,1)",
-          pointBackgroundColor: "#fff",
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
           pointBorderWidth: 1,
           pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(75,192,192,1)",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
